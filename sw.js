1ec2ee8887f65cbf9d53070379a639239ad74d25
kabootarmessanger <kabootarmessanger@gmail.com>
2026-09-19 16:25:49 +0530

    sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+/* Kabootar SOC - Service Worker */
+const CACHE_NAME = 'kabootar-soc-v1';
+const ASSETS = [
+  './admin.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      }).catch(() => caches.match('./admin.html'));
+    })
+  );
+});
